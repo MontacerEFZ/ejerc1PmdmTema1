61bea6dd5c67bd2ff9bfcfc793a922732865c132
montacer <montacer.elfazazielfazazi@cieep.com>
2023-09-25 21:41:03 +0200

    good

--- app/src/main/java/montacer/elfazazi/ejerc1/secondActivity.java
+++ app/src/main/java/montacer/elfazazi/ejerc1/secondActivity.java
@@ -2,7 +2,9 @@ package montacer.elfazazi.ejerc1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class secondActivity extends AppCompatActivity {
 
@@ -10,5 +12,21 @@ public class secondActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+
+        Intent intent = getIntent();
+        Bundle bundle = intent.getExtras();
+        String respuesta = null;
+        int resp;
+        if (bundle.containsKey("LET")){
+            respuesta = bundle.getString("LET");
+            resp = respuesta.length();
+            Toast.makeText(this, "letras: "+resp, Toast.LENGTH_SHORT).show();
+        }
+
+        if (bundle.containsKey("PAL")){
+            respuesta = bundle.getString("PAL");
+            resp = respuesta.split(" ").length;
+            Toast.makeText(this, "Palabras: "+resp, Toast.LENGTH_SHORT).show();
+        }
     }
 }
